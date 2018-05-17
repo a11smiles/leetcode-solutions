d90b522b96cd7b56e72fe26d2672291870763bf5
Joshua Davis <me@jdav.is>
2018-05-17 13:08:53 -0400

    adding problem 4

---
+++ 02. AddTwoNumbers/csharp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AddTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode l1 = new ListNode(9) {
+                next = new ListNode(4) {
+                    next = new ListNode(1)
+                }
+            };
+
+            ListNode l2 = new ListNode(3) {
+                next = new ListNode(0) {
+                    next = new ListNode(2)
+                }
+            };
+
+            var solution = new Solution();
+            var result = solution.AddTwoNumbers(l1, l2);
+        }
+    }
+
+    class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            return Add(l1, l2, 0);
+        }
+
+        private ListNode Add(ListNode l1, ListNode l2, int carry)
+        {
+            if (l1 == null && l2 == null && carry == 0) return null;
+            var value = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
+            return new ListNode(value % 10) { next = Add(l1?.next, l2?.next, value / 10) };
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+}
---
+++ 04. MedianOfTwoSortedArrays/csharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace MedianOfTwoSortedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            int[] nums1 = new int[] { 1, 3 };
+            int[] nums2 = new int[] { 2 };
+            var result = solution.FindMedianSortedArrays(nums1, nums2);
+
+            nums1 = new int[] { 1, 2 };
+            nums2 = new int[] { 3, 4 };
+            result = solution.FindMedianSortedArrays(nums1, nums2);
+
+            nums1 = new int[] { 4, 5, 6, 8, 9 };
+            nums2 = new int[] { };
+            result = solution.FindMedianSortedArrays(nums1, nums2);
+        }
+    }
+
+    class Solution
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int[] combined = nums1.Concat(nums2).ToArray();
+            Array.Sort(combined);
+
+            if (combined.Length % 2 == 0)
+                return (combined[combined.Length / 2] + combined[combined.Length / 2 - 1]) / 2.0;
+            else
+                return combined[combined.Length / 2];
+        }
+    }
+}
